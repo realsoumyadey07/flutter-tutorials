558c7f3d905354a46ff65ae1d50ec82141162290
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-01-08 14:16:27 +0530

    data types and variables

---
+++ dart/variables-datatype.dart
@@ -0,0 +1,18 @@
+//declaration of variable
+
+int? age; //nullable
+BigInt? bigAge;
+double? decimal;
+num floatVal = 5.7;
+String name = "soumya";
+String description = 'Hi this is soumya dey';
+bool isAdult = true;
+
+void main() {
+  age = 22;
+  decimal = 22.4;
+  bigAge = BigInt.parse('3457984526985629856285628563285623648562385763458');
+  decimal = 34.56546593485;
+  name = "Dey";
+  print(decimal);
+}
